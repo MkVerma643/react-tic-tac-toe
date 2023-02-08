def07a62f3dccee250ca3283d9893512d9929e52
Mukesh Verma <mukeshverma643@gmail.com>
2023-02-08 18:35:42 +0530

    Begin with the Game, added the basic library Required for the Game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
+import Icon from './components/Icon';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap';
+
+const itemArray = new Array(9).fill("empty");
 
 function App() {
+
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> */}
+        
+        <Icon/>
+        
       </header>
     </div>
   );
